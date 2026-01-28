71421fdffda4f9d8492f435494a2de5233a778f7
tistrand <tistrand@kth.se>
2026-01-28 15:30:06 +0100

    done

--- hw1/tee.c
+++ hw1/tee.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-
+#define BUFFERSIZE 2
 //create threads
 pthread_t readId;
 pthread_t fileWriteId;
@@ -14,40 +14,45 @@ pthread_t outWriteId;
 
 pthread_cond_t cv; 
 pthread_mutex_t mutex;
-pthread_mutex_t done_lock;
 
-int fileWorking;
-int outWorking;
-char buffer[100];
+
+int fileDone = 0;
+int outDone = 0;
+int addLinePosition = 0;
+int fetchLinePosition = 0;
+char buffer[BUFFERSIZE][100];
+
 bool done = false;
 
 void* Read(void* arg){
-    
-    while(!done){
-        char line[100];
-        //printf("type something :");
-        //fflush(stdout);
+    char line[100];
+
+    //read tar in text, lock, lägger på write pos, ökar writepos väcker
+    //write, kopierar det på rätt pos, om den andra också har gjort det, ökar read pos 
+    //
+
+    while(1){
         fgets(line, sizeof(line), stdin);
-        //printf("%s",line);
+
         pthread_mutex_lock(&mutex);
-        while(fileWorking == 1 || outWorking == 1){
+         if(strcmp(line, "quit prog\n") == 0){
+            fflush(stdout);
+            done = true;
+            pthread_cond_broadcast(&cv);
+            pthread_mutex_unlock(&mutex);
+            break;
+         }
+        /*måsta kolla så att vi inte skriver över */
+        while((addLinePosition+1)%BUFFERSIZE == fetchLinePosition%BUFFERSIZE){
             pthread_cond_wait(&cv, &mutex);
         }
-        
-        strcpy(buffer, line);
+        strcpy(buffer[(addLinePosition%BUFFERSIZE)], line); 
+        addLinePosition +=1;
         pthread_cond_broadcast(&cv);
-        fileWorking = 1;
-        outWorking = 1;
         pthread_mutex_unlock(&mutex);
-
-        
-        //lås och spara buffer i critical section
-        //ready to read och broadcast
-        //vänta att båsa har läst innan nästa skrivs
 
     }
     return NULL;
-
 }
 
 void* FileWrite(void* arg){
@@ -56,32 +61,36 @@ void* FileWrite(void* arg){
     FILE* file_ptr = fopen(filename, "w");
     char local_bfr[100];
 
-    //vänta på att buffern fylls
-    while(!done)
-    {
+    while(1)
+    {   
         pthread_mutex_lock(&mutex);
-        while(fileWorking == 0){
-            pthread_cond_wait(&cv, &mutex);
+        while((addLinePosition%BUFFERSIZE == fetchLinePosition%BUFFERSIZE || fileDone == 1 ) && !done) {
+            pthread_cond_wait(&cv, &mutex); //waiting to get woken up
         }
-        strcpy(local_bfr, buffer);
-        fileWorking = 0;
-        if(fileWorking == 0 && outWorking == 0){
-            pthread_cond_signal(&cv);
+        
+        if(done){
+            pthread_mutex_unlock(&mutex);
+            fflush(stdout);
+            break;
         }
+        strcpy(local_bfr, buffer[fetchLinePosition%BUFFERSIZE]); //copies from right spot in buffer
+        fileDone = 1;
+        if(fileDone == 1 && outDone == 1){
+            fetchLinePosition +=1; 
+            fileDone = 0;
+            outDone = 0;
+        }
+
         pthread_mutex_unlock(&mutex);
-
         fprintf(file_ptr, "%s", local_bfr); 
-
-        //printf("writes to file, %s", local_bfr);
-
-        //writes line to file's internal buffer
-        //may not need fflush... ?
         fflush(file_ptr); //forces all the content in the buffer to be written to disk (make content visible instantly)
+
     
     }  
     
     fclose(file_ptr);
-    printf("closed file");
+    printf("closed file\n");
+    fflush(stdout);
     return NULL;
 
 }
@@ -89,21 +98,29 @@ void* FileWrite(void* arg){
 void* OutWrite(void* arg){
 
     char local_bfr[100];
-    while(!done)
+    while(1)
     {
         pthread_mutex_lock(&mutex);
-        while(outWorking == 0){
-            pthread_cond_wait(&cv, &mutex);
+        while((addLinePosition%BUFFERSIZE == fetchLinePosition%BUFFERSIZE || outDone == 1) && !done){
+            pthread_cond_wait(&cv, &mutex); //waiting to get woken up
         }
-        strcpy(local_bfr, buffer);
-        outWorking = 0;
-        if(fileWorking == 0 && outWorking == 0){
+
+        if(done){
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        strcpy(local_bfr, buffer[fetchLinePosition%BUFFERSIZE]);
+        outDone = 1;
+        if(fileDone == 1 && outDone == 1){
+            fetchLinePosition +=1; 
+            fileDone = 0;
+            outDone = 0;
             pthread_cond_signal(&cv);
         }
         pthread_mutex_unlock(&mutex);
         printf("print to output: %s", local_bfr);
         fflush(stdout);
-
+        
     }  
     
     return NULL;
@@ -112,7 +129,11 @@ void* OutWrite(void* arg){
 
 int main(int argc, char* argv[]){
     char* filename = argv[1];
-    printf("run main with file %s\n", filename);
+    printf("running with file %s\n", filename);
+    printf("type quit prog to end the program\n");
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cv, NULL);
 
     pthread_create(&readId, NULL, Read, NULL);
     pthread_create(&fileWriteId, NULL, FileWrite, filename);
@@ -121,6 +142,8 @@ int main(int argc, char* argv[]){
     pthread_join(readId, NULL);
     pthread_join(fileWriteId, NULL);
     pthread_join(outWriteId, NULL);
-    return 0;
+    printf("quit program main\n");
+    fflush(stdout);
+
     //pthread_exit(NULL);
 }
